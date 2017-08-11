5eae4926c042b67c6f2d0f44a67db469507016c0
Clément Nerma <clement.nerma@gmail.com>
2017-08-11 19:28:51 +0200

    Make the bootloader

---
+++ userland/sys/boot/bootloader.js
@@ -0,0 +1,8 @@
+// Enable strict mode
+"use strict";
+
+// NOTE: This file is runned in the main frame's global context, so it can
+//       access any ressource that has been declared in that context.
+
+// Boot message
+bootLine('Bootloader started.');
