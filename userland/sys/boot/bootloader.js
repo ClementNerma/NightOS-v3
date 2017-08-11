a333c50453cdd56929b7f51ea957655af901f351
Clément Nerma <clement.nerma@gmail.com>
2017-08-11 19:29:59 +0200

    Wrap bootloader inside an IIFE

--- userland/sys/boot/bootloader.js
+++ userland/sys/boot/bootloader.js
@@ -4,5 +4,9 @@
 // NOTE: This file is runned in the main frame's global context, so it can
 //       access any ressource that has been declared in that context.
 
-// Boot message
-bootLine('Bootloader started.');
+// Wrap the bootloader's code inside an IIFE to avoid polluating the global env.
+(function () {
+  // Boot message
+  bootLine('Bootloader started.');
+
+})(); // Invoke the bootloader's code as a functino
