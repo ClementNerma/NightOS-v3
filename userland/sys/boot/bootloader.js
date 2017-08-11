b5db98531179237d860a6a89787119caf1fe6bf5
Clément Nerma <clement.nerma@gmail.com>
2017-08-11 19:31:58 +0200

    Allow the system to boot after 3 seconds

--- userland/sys/boot/bootloader.js
+++ userland/sys/boot/bootloader.js
@@ -9,4 +9,22 @@
   // Boot message
   bootLine('Bootloader started.');
 
+  /**
+   * Boot normally
+   * @returns {void}
+   */
+  function boot () {
+    // Has to be made
+  }
+
+  // Make a timer to boot normally if no key has been pressed in 3 seconds.
+  // NOTE: The timer is saved in a variable to be canceled if a key is pressed
+  //       during the short delay.
+  let normalBootTimer = setTimeout(
+    // No key was pressed ; boot normally
+    boot,
+    // 3s wait
+    3000
+  );
+
 })(); // Invoke the bootloader's code as a functino
