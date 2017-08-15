2341545a2d663596c91afc414ae70903e55e2a26
Clément Nerma <clement.nerma@gmail.com>
2017-08-15 21:51:57 +0200

    Add a few notes to the sysloader

--- userland/sys/boot/sysloader.js
+++ userland/sys/boot/sysloader.js
@@ -1,5 +1,13 @@
 // Enable strict mode
 "use strict";
 
+// NOTE: This file is runned in the main frame's global context, so it can
+//       access any ressource that has been declared in that context.
+
+// NOTE: It's useless to wrap the bootloader's code inside an IIFE to avoid
+//       polluating the global environment because ressources are defined using
+//       the `let` statement, which makes them defined only in this file's
+//       scope.
+
 // Boot message
-bootLine('System loader is running.');
+bootLine('Sysloader has been loaded.');
