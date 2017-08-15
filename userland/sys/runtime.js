bca2ee0b662cf9a5d2ed87362817984de2e9966c
Clément Nerma <clement.nerma@gmail.com>
2017-08-15 22:01:12 +0200

    Check if the registry exists

--- userland/sys/runtime.js
+++ userland/sys/runtime.js
@@ -3,3 +3,13 @@
 
 // Boot message
 bootLine('Launching desktop environment...');
+
+/* Open the registry to get the desktop environment's (DE's) application name */
+
+// Save the registry's path in the memory
+let regPath = path.join(__dirname /* Main frame's CWD */, '..', 'etc', 'registry');
+
+// Check if the registry is available
+if (! fs.existsSync(regPath) || ! fs.lstatSync(regPath).isFile())
+  // Throw an error
+  error('Registry file is missing');
